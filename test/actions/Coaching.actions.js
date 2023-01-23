d1a2c035e8368a3204e048dd1aebef1884286fd3
Pavelas <pmoisevicius@shift4.com>
2023-01-23 11:33:17 +0200

    Refactor functions

--- test/actions/Coaching.actions.js
+++
@@ -1,8 +0,0 @@
-// const Page = require('../page/Coaching.page.js');
-import Page from '../page/Coaching.page.js'
-class openURL extends Page {
-    open(urlName) {
-        return super.open(`${urlName}`);
-    }
-}
-module.exports = new openURL();
